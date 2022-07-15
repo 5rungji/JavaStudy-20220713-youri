ccc3164a23fb4c5d72d0f54c4217b3b1103a9560
5ranG <opel22@naver.com>
2022-07-15 11:05:47 +0900

    챕터1 출력

---
+++ src/j01_출력/PrintTest.java
@@ -0,0 +1,13 @@
+package j01_출력;
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		System.out.print("Hello, Java!");
+		System.out.print("유리\n\n");
+
+		System.out.println("Hello, Java!");
+		System.out.println("유리");
+	}
+
+}
