df319e17f221314e3f5df40c45213d446b231f01
5ranG <opel22@naver.com>
2022-07-15 12:48:19 +0900

    챕터5 입력

---
+++ src/j05_입력/Input1.java
@@ -0,0 +1,31 @@
+package j05_입력;
+
+import java.util.Scanner;
+
+public class Input1 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		//Scan + ctrl + space
+
+		String str = null;
+		String str2 = null;
+		int num1 = 0;
+		
+		System.out.print("내용을 입력하세요: ");
+		str = scanner.next(); 
+		// 문자열 입력
+		// next: 띄워쓰기 줄바꿈 불포함
+		System.out.println("출력: " + str);
+		
+		System.out.print("내용을 입력하세요: ");
+		str2 = scanner.next();
+		//nextLine: 띄워쓰기, 줄바꿈 포함
+		System.out.println("출력2: " + str2);
+
+		System.out.print("내용을 입력하세요: ");
+		num1 = scanner.nextInt();
+		System.out.println("출력3: " + num1);
+		
+	}
+}
