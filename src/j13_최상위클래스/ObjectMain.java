56f487fab47fb59d5e464d0694ede7b8e150d17e
5ranG_HOME <opel22@naver.com>
2022-08-03 02:15:36 +0900

    챕터13. 최상위클래스

---
+++ src/j13_최상위클래스/ObjectMain.java
@@ -0,0 +1,17 @@
+package j13_최상위클래스;
+
+public class ObjectMain {
+	
+	public static void main(String[] args) {
+		Object object = new Object();	
+		
+		Object test = new Test();
+		((Test) test).testMethod();
+		
+		//모든 클래스는 오브젝트 클래스를 가진다.
+		//상속의 형태로
+		
+		
+	}
+
+}
