5890c6eaeff782eae488d705ded998091bee3a42
5ranG_HOME <opel22@naver.com>
2022-08-03 02:16:54 +0900

    챕터15. 스태틱

---
+++ src/j15_스태틱/MainStaticTest.java
@@ -0,0 +1,14 @@
+package j15_스태틱;
+
+public class MainStaticTest {
+
+	public void prinTest() {
+		System.out.println("출력");
+	}
+	
+	public static void main(String[] args) {
+		MainStaticTest mainStaticTest = new MainStaticTest();
+		mainStaticTest.prinTest();
+	}
+
+}
