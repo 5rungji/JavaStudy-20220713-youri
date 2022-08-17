ab64f6952318a714be2080d7bbe502afe8e81488
5ranG_NB <opel22@naver.com>
2022-08-17 17:40:19 +0900

    0817 복습

--- src/j19_익명클래스/Main.java
+++ src/j19_익명클래스/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		AbstractTest abstractTest = new AbstractTestImpl();
-		abstractTest.add(2, 5);
-
-		
-
-		
+		abstractTest.add(2, 5);		
 	}
 }
